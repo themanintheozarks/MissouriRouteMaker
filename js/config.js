7c27dbc6559e627961eb08e314261931b0ded139
themanintheozarks <themanintheozarks@gmail.com>
2026-07-19 16:21:44 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,28 @@
+/*
+==========================================================
+Missouri Route Maker
+
+config.js
+
+Global Application Configuration
+==========================================================
+*/
+
 export const APP_CONFIG = {
+
     APP_NAME: "Missouri Route Maker",
-    DEFAULT_LATITUDE: 38.573936,
-    DEFAULT_LONGITUDE: -92.603760,
-    DEFAULT_ZOOM: 7,
+
+    VERSION: "0.1.0",
+
+    DEFAULT_CENTER: {
+        lat: 38.573936,
+        lng: -92.603760
+    },
+
+    DEFAULT_ZOOM: 6,
+
     ARRIVAL_RADIUS_FEET: 200,
-    GPS_POLL_RATE_MS: 5000
+
+    GPS_POLL_RATE: 5000
+
 };
