07061e312afc2551f2c5b7de00096e73e52e7f69
themanintheozarks <themanintheozarks@gmail.com>
2026-07-22 18:30:02 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ Application Entry Point
 
 import { initializeMap } from "./map/map.js";
 import { initializeGPS } from "./gps.js";
-import { initializePlaces } from "./places.js";
 
 /*
 ==========================================================
@@ -25,40 +24,7 @@ function initializeApplication() {
 
     initializeMap();
 
-    /*
-    ==========================================
-    Wait until the map is fully loaded before
-    initializing modules that depend on it.
-    ==========================================
-    */
-
-    window.map.on("load", () => {
-
-        initializeGPS();
-
-        initializePlaces();
-
-    });
-
-    registerServiceWorker();
-
-}
-
-/*
-==========================================================
-Service Worker
-==========================================================
-*/
-
-function registerServiceWorker() {
-
-    if (!("serviceWorker" in navigator)) {
-        return;
-    }
-
-    navigator.serviceWorker.register(
-        "./service-worker.js"
-    );
+    initializeGPS();
 
 }
 
