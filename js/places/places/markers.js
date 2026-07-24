ff2bf60a7cd56bf30ab537aa30885bb53ccaf50a
themanintheozarks <themanintheozarks@gmail.com>
2026-07-24 10:41:01 -0500

    Create markers.js

---
+++ js/places/places/markers.js
@@ -0,0 +1,19 @@
+/*
+==========================================================
+Missouri Route Maker
+
+markers.js
+
+Marker Management
+
+==========================================================
+*/
+
+export function drawPlaceMarker(place) {
+
+    console.log(
+        "Marker Manager Ready:",
+        place.name
+    );
+
+}
