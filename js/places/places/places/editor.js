931da222435998f685cd6a53ed80205f9ef46782
themanintheozarks <themanintheozarks@gmail.com>
2026-07-24 10:41:26 -0500

    Create editor.js

---
+++ js/places/places/places/editor.js
@@ -0,0 +1,18 @@
+/*
+==========================================================
+Missouri Route Maker
+
+editor.js
+
+Place Editor
+
+==========================================================
+*/
+
+export function openPlaceEditor() {
+
+    console.log(
+        "Place Editor Ready"
+    );
+
+}
